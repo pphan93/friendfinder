64cea6ba35d2586f1f08e51c6f1164b085e23e49
Phuoc Phan <phuocphan12@gmail.com>
2019-04-15 22:20:44 -0400

    logic for match is done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,24 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+//console.log(friends)
 
-  app.get("/api/friends", function(req, res) {
+
+module.exports = function (app) {
+
+  app.get("/api/friends", function (req, res) {
+    console.log(friends)
     res.json(friends);
   });
 
 
-  app.post("/api/friends", function(req, res) {
-    var data = req.body
-    console.log(data)
+  app.post("/api/friends", function (req, res) {
+    var data = req.body;
+    console.log(data);
+    res.json(closestMatch(data.scores))
+    friends.push(data)
   });
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
@@ -20,3 +26,51 @@ module.exports = function(app) {
     res.json({ ok: true });
   });
 };
+
+function closestMatch(userInput) {
+  var match = [];
+  friends.forEach(element => {
+    var friendArrary = element.scores
+    var difference = totalDifference(userInput, friendArrary)
+    console.log(difference)
+    if (match.length == 0) {
+      match[0] = {
+        "totalDiff": difference,
+        "name": element.name,
+        "photo": element.photo
+      }
+
+    } else if (match[0]["totalDiff"] > difference) {
+      match[0] = {
+        "totalDiff": difference,
+        "name": element.name,
+        "photo": element.photo
+      }
+    }
+    console.log("HAHA")
+    console.log(match)
+  });
+
+  console.log("---------------------")
+  return(match[0])
+}
+
+function totalDifference(userInput, friendArray) {
+  var difference = []
+  //console.log(userInput)
+  for (var i = 0; i < userInput.length; i++) {
+    //console.log("_____")
+    //console.log(Math.abs(userInput[i] - friendArray[i]))
+    difference.push(Math.abs(userInput[i] - friendArray[i]));
+  }
+
+
+  function getSum(total, num) {
+    return total + num;
+  }
+
+  console.log(difference)
+
+  return difference.reduce(getSum);
+
+}
